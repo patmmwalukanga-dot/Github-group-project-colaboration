679bf35cf1a91a2f7a30f6e39d273c77c953f98e
patmmwalukanga-dot <pat.m.mwalukanga@gmail.com>
2026-06-08 21:30:08 +0200

    my first upload

---
+++ get_product.php
@@ -0,0 +1,20 @@
+<?php
+include 'db_config.php';
+
+$id = $_GET['id'];
+
+if(!$id) {
+    echo json_encode(["error" => "Product ID is required"]);
+    exit;
+}
+
+$sql = "SELECT * FROM products WHERE id='$id' AND isSold=0";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    echo json_encode($row);
+} else {
+    echo json_encode(["error" => "Product not found"]);
+}
+?>
